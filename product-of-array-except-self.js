e696e48e316c90187c0ebdbf50185397d2a07d8c
AndriyCh1 <chekanovskiyacc@gmail.com>
2024-05-26 22:51:17 +0300

    238. Product of Array Except Self

---
+++ product-of-array-except-self.js
@@ -0,0 +1,62 @@
+/*
+  238. Product of Array Except Self
+
+  Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+  The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+  You must write an algorithm that runs in O(n) time and without using the division operation.
+
+  Example 1:
+  Input: nums = [1,2,3,4]
+  Output: [24,12,8,6]
+
+  Example 2:
+  Input: nums = [-1,1,0,-3,3]
+  Output: [0,0,9,0,0]
+
+  Constraints:
+  2 <= nums.length <= 105
+  -30 <= nums[i] <= 30
+
+  The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+  Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+  const result = [];
+
+  let leftProducts = 1;
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = leftProducts;
+    leftProducts *= nums[i];
+  }
+
+  let rightProducts = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] *= rightProducts;
+    rightProducts *= nums[i];
+  }
+
+  return result;
+};
+
+console.log(productExceptSelf([1, 2, 3, 4]));
+
+/* 
+    NOTE: The fastest solution:
+  var productExceptSelf = function(nums) {
+      let ans = new Array(nums.length).fill(1);
+      for (let i = 1; i < ans.length; i++) {
+          ans[i] = ans[i-1] * nums[i-1];
+      }
+      let rightProduct = 1;
+      for (let i = ans.length-2; i >= 0; i--) {
+          ans[i] = ans[i] * nums[i+1] * rightProduct;
+          rightProduct *= nums[i+1];
+      }
+      return ans; 
+  };
+*/
